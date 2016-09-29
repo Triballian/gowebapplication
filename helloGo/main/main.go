4a1368df4e03ba2faf27b039a59653dcb5d6df59
Triballian <candiapps@gmail.com>
2016-09-29 13:47:19 -0500

    added buffering

--- helloGo/main/main.go
+++ helloGo/main/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"io/ioutil"
+	"bufio"
+
 	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -18,9 +21,29 @@ type MyHandler struct {
 
 func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	path := "public/" + req.URL.Path
-	data, err := ioutil.ReadFile(string(path))
+	f, err := os.Open(path)
+
 	if err == nil {
-		w.Write(data)
+		bufferedReader := bufio.NewReader(f)
+		var contentType string
+
+		if strings.HasSuffix(path, ".css") {
+			contentType = "text/css"
+		} else if strings.HasSuffix(path, ".html") {
+			contentType = "text/html"
+		} else if strings.HasSuffix(path, ".js") {
+			contentType = "application/javascript"
+		} else if strings.HasSuffix(path, ".png") {
+			contentType = "image/png"
+		} else if strings.HasSuffix(path, ".mp4") {
+			contentType = "video/mp4"
+		} else {
+			contentType = "text/plain"
+		}
+
+		w.Header().Add("Content Type", contentType)
+
+		bufferedReader.WriteTo(w)
 	} else {
 		w.WriteHeader(404)
 		w.Write([]byte("4004 - " + http.StatusText(404)))

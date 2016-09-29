26ad2261527e85358a9f951db58a5945c980042f
Triballian <candiapps@gmail.com>
2016-09-29 15:59:23 -0500

    using child templates

--- helloGo/main/main.go
+++ helloGo/main/main.go
@@ -8,31 +8,42 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Add("Content Type", "text/html")
-		tmpl, err := template.New("test").Parse(doc)
-		if err == nil {
-			context := Context{
-				[3]string{"Lemon", "Orange", "Apple"},
-				"the title",
-			}
+		templates := template.New("template")
+		templates.New("test").Parse(doc)
+		templates.New("header").Parse(header)
+		templates.New("footer").Parse(footer)
 
-			tmpl.Execute(w, context)
+		context := Context{
+			[3]string{"Lemon", "Orange", "Apple"},
+			"the title",
 		}
+
+		templates.Lookup("test").Execute(w, context)
+
 	})
 	http.ListenAndServe(":8000", nil)
 
 }
 
 const doc = `
-<!DOCTYPE html>
-<html>
-	<head><title>{{.Title}}</title></head>
+{{template "header" .Title}}
 	<body>
 		<h1>List of Fruit</h1>
 		<ul>
 		{{range .Fruit}}
 			<li>{{.}}</li>
 		{{end}}
+		</ul>
 	</body>
+{{template "footer"}}
+`
+const header = `
+<!DOCTYPE html>
+<html>
+	<head><title>{{.}}</title></head>
+
+`
+const footer = `
 </html>
 `
 
